c051892090bf1444279d30e45f5fd2efdd753582
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2024-02-05 21:08:40 +0100

    Add project files.

---
+++ Uppgift 3 list inl/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift_3_Listor_inl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> area = new Dictionary<string, int>();
+            area["sverige"] = 450;
+            area["norge"] = 385;
+            area["danmark"] = 43;
+            area["finland"] = 338;
+            string land = Console.ReadLine();
+            Console.WriteLine(area[land]);
+
+        }
+
+    }
+}
